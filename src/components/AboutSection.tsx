08911c21e8947794a9eae297482a9874b9dc57f0
Daksh Vohra <67052127+kermitsgonnakermicide@users.noreply.github.com>
2025-08-30 19:38:32 +0530

    Make AboutSection significantly bigger

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export function AboutSection() {
   return (
-    <div className="bg-portfolio-cream rounded-2xl p-6 md:p-16">
-      <p className="text-base md:text-xl text-portfolio-blue leading-relaxed">
+    <div className="bg-portfolio-cream rounded-2xl p-8 md:p-24">
+      <p className="text-lg md:text-2xl text-portfolio-blue leading-relaxed">
 Hi, I'm Daksh and I like to work with robots and other fun computer-related things. I've dabbled in a ton of different fields, from app development to web development to PCB design, 3D Printing, CAD, LEGO Robotics, Microcontrollers and MATLAB and so so much more. I'm interested in working on new and emerging technologies, especially in the robotics space. Currently, I'm in Grade 10(thats sophomore year) at Shiv Nadar School and I'm based in the Delhi-NCR area. I'm excited to work on cool robotics projects in the future!
       </p>
     </div>
